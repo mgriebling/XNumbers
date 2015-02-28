c8ca160692a2af29fa9806dcebf3606ad33606ce
Michael Griebling <bob@c-inspirations.com>
2015-02-28 16:48:21 -0500

    Initial Commit

---
+++ XNumbers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  XNumbers
+//
+//  Created by Mike Griebling on 28 Feb 2015.
+//  Copyright (c) 2015 Computer Inspirations. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
